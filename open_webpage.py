4030e0a9cc7ce9c36e9dea47906c1dcbae1b51a2
omnicode0 <63731332+omnicode0@users.noreply.github.com>
2020-07-04 12:12:42 -0400

    Version 1 primary file
    Trying to make a Python program to open a user's browser to the census data download webpage.

---
+++ open_webpage.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import webbrowser
+
+yesOrNo = None #initializing variable containing whether user wants to open the webpage or not
+
+print('Welcome! This program will navigate you to the area on the Census website where you can download data to suit your needs.')
+print()
+
+while yesOrNo != 'yes' or yesOrNo != 'no':
+yesOrNo = str(input('Would you like me to open the webpage in your default browser? say "yes" or "no": '))
+
+if yesOrNo == 'yes':
+  print('It looks like you want us to open the webpage! Here you go.')
+  webbrowser.open('https://www.census.gov/data/tables.html')
+  
+if yesOrNo == 'no':
+  print('We We hope you have a nice day!')
+
+else:
+  print('I couldn\'t understand you! What did you mean? Please respond in lower case and no spaces "yes" or "no"')
