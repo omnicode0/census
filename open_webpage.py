e9776269dc589dbe2be9aa1a555709bda84cbef4
omnicode0 <63731332+omnicode0@users.noreply.github.com>
2020-07-04 13:01:32 -0400

    Fix the endless loop
    Adding break statements and turning the second if to an elifshould exit the loop

--- open_webpage.py
+++ open_webpage.py
@@ -13,9 +13,11 @@ while yesOrNo != 'yes' or yesOrNo != 'no':
   if yesOrNo == 'yes':
     print('It looks like you want us to open the webpage! Here you go.')
     webbrowser.open('https://www.census.gov/data/tables.html')
+    break
   
-  if yesOrNo == 'no':
+  elif yesOrNo == 'no':
     print('We hope you have a nice day!')
+    break
 
   else:
     print('I couldn\'t understand you! What did you mean? Please respond in lower case and no spaces "yes" or "no"')
